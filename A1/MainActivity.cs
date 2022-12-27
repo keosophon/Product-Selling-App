c89a826c7a1a7d1f2ec8a5e336be27d7ff6ad649
Sophon Keo <54705746+keosophon@users.noreply.github.com>
2022-12-28 12:46:56 +1300

    add registration UI and its code

--- A1/MainActivity.cs
+++ A1/MainActivity.cs
@@ -2,24 +2,47 @@
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
+using Android.Widget;
+using Android.Views;
+using Android.Content;
 
 namespace A1
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private TextView forgotPasswordTextView;
+        private TextView registerTextView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            //bind variables to UI elements
+            forgotPasswordTextView = FindViewById<TextView>(Resource.Id.txtForgotPassword);
+            registerTextView = FindViewById<TextView>(Resource.Id.txtRegisterNow);
+
+            forgotPasswordTextView.Click += delegate
+            {
+                StartActivity(typeof(RegistrationActivity));
+            };
+
+            registerTextView.Click += delegate
+            {
+                StartActivity(typeof(RegistrationActivity));
+            };
         }
+        
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+        
     }
 }
---
+++ A1/RegistrationActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace A1
+{
+    [Activity(Label = "Activity1")]
+    public class RegistrationActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.activity_registration);
+        }
+    }
+}
