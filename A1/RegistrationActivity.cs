40fac8c9c4eb28e822c4a61d6321f98f14802a94
Sophon Keo <54705746+keosophon@users.noreply.github.com>
2022-12-30 12:04:28 +1300

    Improve UI consistency

--- A1/RegistrationActivity.cs
+++ A1/RegistrationActivity.cs
@@ -15,13 +15,25 @@ namespace A1
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
     public class RegistrationActivity : AppCompatActivity
     {
+        private Spinner month;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
             SetContentView(Resource.Layout.activity_registration);
-            //this.SetTitle(Resource.String.app_name);
+
+            //bind variables to UI elements
+            month = FindViewById<Spinner>(Resource.Id.spMonth);
+
+            //adapter is the bridge between spinner and data.
+            //adapter needs both data and its data drop down layout
+            ArrayAdapter adapter = ArrayAdapter.CreateFromResource(this, Resource.Array.month, Android.Resource.Layout.SimpleSpinnerItem);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            month.Adapter = adapter;
+
+
+
         }
     }
 }
