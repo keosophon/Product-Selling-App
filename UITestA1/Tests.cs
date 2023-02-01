54043e242e5ab8edf1746677ab9e8d184f38745b
Sophon Keo <54705746+keosophon@users.noreply.github.com>
2023-02-01 13:29:12 +1300

    add test

--- UITestA1/Tests.cs
+++ UITestA1/Tests.cs
@@ -28,14 +28,15 @@ namespace UITestA1
         [Test]
         public void LogIngPageDisplayed()
         {
-            AppResult[] results = app.WaitForElement(c => c.Marked("LogInPage"));
-            //app.Screenshot("Sign In");
+            //when app started, find LogInPage title on Login Page
+            AppResult[] results = app.WaitForElement(c => c.Marked("LogInPage"));            
             Assert.IsTrue(results.Any());
         }
 
         [Test]
         public void TestRegistrationPageDisplayed()
         {
+            //on Login Page, click RegisterNow link to open Registration Page
             app.Tap("txtRegisterNow");
             AppResult[] results = app.WaitForElement(c => c.Marked("RegistrationPage"));
             Assert.IsTrue(results.Any());
@@ -44,6 +45,7 @@ namespace UITestA1
         [Test]
         public void TestDashboardDisplayed()
         {
+            //login page enter email address and password, and click button login
             app.WaitForElement(c => c.Id("txtEmailPhoneLog"));
             app.EnterText(c => c.Id("txtEmailPhoneLog"), "keosophon8888@gmail.com");
 
@@ -51,6 +53,8 @@ namespace UITestA1
             app.EnterText(c => c.Id("txtPassowrdLog"), "hello123");            
 
             app.Tap("btnLogin");
+
+            //on Dashboard page, find Dashboard Text
             AppResult[] results = app.WaitForElement(c => c.Id("txtDashBoard"));
             Assert.IsTrue(results.Any());
         }
@@ -59,14 +63,18 @@ namespace UITestA1
         [Test]
         public void TestLogout()
         {
+            //login page enter email address and password, and click button login
             app.WaitForElement(c => c.Id("txtEmailPhoneLog"));
             app.EnterText(c => c.Id("txtEmailPhoneLog"), "keosophon8888@gmail.com");
 
             app.WaitForElement(c => c.Id("txtPassowrdLog"));
             app.EnterText(c => c.Id("txtPassowrdLog"), "hello123");
-
             app.Tap("btnLogin");
+
+            //on dahboard page, click Log Out to logout and go to login page
             app.Tap("txtLogOut");
+
+            //on login page, find LoginPage Title
             AppResult[] results = app.WaitForElement(c => c.Marked("LogInPage"));
             Assert.IsTrue(results.Any());
         }
@@ -74,14 +82,18 @@ namespace UITestA1
         [Test]
         public void TestProductPageDisplayed()
         {
+            //login page enter email address and password, and click button login
             app.WaitForElement(c => c.Id("txtEmailPhoneLog"));
             app.EnterText(c => c.Id("txtEmailPhoneLog"), "keosophon8888@gmail.com");
 
             app.WaitForElement(c => c.Id("txtPassowrdLog"));
             app.EnterText(c => c.Id("txtPassowrdLog"), "hello123");
-
             app.Tap("btnLogin");
+
+            //on dahboard page, click button Detail1 of Product1
             app.Tap("btnDetail1");
+
+            //on product page view, find button BuyNow
             AppResult[] results = app.WaitForElement(c => c.Marked("btnBuyNow"));
             Assert.IsTrue(results.Any());
         }
