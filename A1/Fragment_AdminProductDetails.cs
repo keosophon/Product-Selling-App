5595498f4544059417cd63a5bd0042bfd2b31273
Sophon Keo <54705746+keosophon@users.noreply.github.com>
2023-02-12 10:53:15 +1300

    ...

--- A1/Fragment_AdminProductDetails.cs
+++ A1/Fragment_AdminProductDetails.cs
@@ -15,15 +15,22 @@ namespace A1
     
     public class Fragment_AdminProductDetails : AndroidX.Fragment.App.Fragment
     {
+        private TextView txtProductIdAdmin;
+        private TextView txtProductNameAdmin;
+        private TextView txtProductPriceAdmin;
+        private TextView txtProductStockAdmin;
+        private TextView txtProductDescriptionAdmin;
+        private TextView txtProductImageSmallUrl;
+        private TextView txtProductImageBigUrl;
+        private Button btnAdd;
         
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your fragment here
-        }
-
-        
+            
+        }       
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             // Use this to return your custom view for this Fragment
